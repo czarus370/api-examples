cd251db0844a9b0ab8c863667e62f144cda0fb99
- Cezary - <czarus370@gmail.com>
2025-05-12 07:00:47 +0200

    Create api.py

---
+++ api.py
@@ -0,0 +1,45 @@
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+import base64
+import os
+from google import genai
+from google.genai import types
+
+
+def generate():
+    client = genai.Client(
+        api_key="AIzaSyBsmFlo61KkTJC8OFFYhm-LS0mCYPjI7ao",
+    )
+
+    model = "gemini-2.5-pro-preview-05-06"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="""INSERT_INPUT_HERE"""),
+            ],
+        ),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        response_mime_type="text/plain",
+    )
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        print(chunk.text, end="")
+
+if __name__ == "__main__":
+    generate()
+
+
+
+
+
+
+
+
+
